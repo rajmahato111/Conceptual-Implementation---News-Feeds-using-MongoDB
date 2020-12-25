8d85912ba8871efc1087193bd49f0da3da7f3d0a
hexrajkumar <rajkumar.mahto@hexagonglobal.in>
2020-12-25 13:33:04 +0530

    4th commit

--- src/index.js
+++ src/index.js
@@ -4,17 +4,18 @@ const port = 8080
 
 const onePageArticleCount = 10
 
+
 // Parse JSON bodies (as sent by API clients)
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get("/newFeeds", async (req, res) => {
-  const { limit, offset } = req.query;
+app.get("/newFeeds",(req,res)=>{
+    let limit=Number(req.query.limit);
+    let offset=Number(req.query.offset);
+    limit= limit? limit : 10;
+    offset= offset? offset : 0;
 
-  const lim = limit == undefined ? 10 : isNaN(limit) ? 10 : Number(limit);
-  const off = offset == undefined ? 0 : isNaN(offset) ? 0 : Number(offset);
-  const result = await newsArticleModel.find().skip(off).limit(lim);
-  res.send(result);
+    newsArticleModel.find().skip(offset).limit(limit).then((data)=>res.send(data));
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
